1b80f1000fa7845082211e757dc8b75f0335daf8
leonsenqos <leonsp@enqos.com>
2019-01-02 19:36:56 +0530

    product mapping partial ui

--- WebApp/src/components/product/productCategorySelector.js
+++ WebApp/src/components/product/productCategorySelector.js
@@ -1,38 +1,219 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles/index';
+import deburr from 'lodash/deburr';
+import Autosuggest from 'react-autosuggest';
+import match from 'autosuggest-highlight/match';
+import parse from 'autosuggest-highlight/parse';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import Popper from '@material-ui/core/Popper';
+import { withStyles } from '@material-ui/core/styles';
+
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
+
+const suggestions = [
+    { label: 'Afghanistan' },
+    { label: 'Aland Islands' },
+    { label: 'Albania' },
+    { label: 'Algeria' },
+    { label: 'American Samoa' },
+    { label: 'Andorra' },
+    { label: 'Angola' },
+    { label: 'Anguilla' },
+    { label: 'Antarctica' },
+    { label: 'Antigua and Barbuda' },
+    { label: 'Argentina' },
+    { label: 'Armenia' },
+    { label: 'Aruba' },
+    { label: 'Australia' },
+    { label: 'Austria' },
+    { label: 'Azerbaijan' },
+    { label: 'Bahamas' },
+    { label: 'Bahrain' },
+    { label: 'Bangladesh' },
+    { label: 'Barbados' },
+    { label: 'Belarus' },
+    { label: 'Belgium' },
+    { label: 'Belize' },
+    { label: 'Benin' },
+    { label: 'Bermuda' },
+    { label: 'Bhutan' },
+    { label: 'Bolivia, Plurinational State of' },
+    { label: 'Bonaire, Sint Eustatius and Saba' },
+    { label: 'Bosnia and Herzegovina' },
+    { label: 'Botswana' },
+    { label: 'Bouvet Island' },
+    { label: 'Brazil' },
+    { label: 'British Indian Ocean Territory' },
+    { label: 'Brunei Darussalam' },
+];
+
+function renderInputComponent(inputProps) {
+    const { classes, inputRef = () => {}, ref, ...other } = inputProps;
+
+    return (
+        <TextField
+    className="productnamefield producttxtfield"
+    fullWidth
+    InputProps={{
+        inputRef: node => {
+            ref(node);
+            inputRef(node);
+        },
+            classes: {
+            input: classes.input,
+        },
+        disableUnderline:true,
+    }}
+    {...other}
+    />
+);
+}
+
+function renderSuggestion(suggestion, { query, isHighlighted }) {
+    const matches = match(suggestion.label, query);
+    const parts = parse(suggestion.label, matches);
+
+    return (
+        <MenuItem selected={isHighlighted} component="div">
+        <div>
+        {parts.map((part, index) => {
+            return part.highlight ? (
+                <span key={String(index)} style={{ fontWeight: 500 }}>
+            {part.text}
+        </span>
+        ) : (
+            <strong key={String(index)} style={{ fontWeight: 300 }}>
+            {part.text}
+        </strong>
+        );
+        })}
+</div>
+    </MenuItem>
+);
+}
+
+function getSuggestions(value) {
+    const inputValue = deburr(value.trim()).toLowerCase();
+    const inputLength = inputValue.length;
+    let count = 0;
+
+    return inputLength === 0
+        ? []
+        : suggestions.filter(suggestion => {
+            const keep =
+                count < 5 && suggestion.label.slice(0, inputLength).toLowerCase() === inputValue;
+
+            if (keep) {
+                count += 1;
+            }
+
+            return keep;
+        });
+}
+
+function getSuggestionValue(suggestion) {
+    return suggestion.label;
+}
 
 const styles = theme => ({
-    button: {
-        margin: 40,
-        textTransform: 'capitalize',
+    root: {
+        height: 50,
+        flexGrow: 1,
+        paddingLeft:10,
+        paddingRight:25,
+    },
+    container: {
+        position: 'relative',
+    },
+    suggestionsContainerOpen: {
+        position: 'absolute',
+        zIndex: 1,
+        marginTop: theme.spacing.unit,
+        left: 0,
+        right: 0,
+    },
+    suggestion: {
+        display: 'block',
+    },
+    suggestionsList: {
+        margin: 0,
+        padding: 0,
+        listStyleType: 'none',
+    },
+    divider: {
+        height: theme.spacing.unit * 2,
     },
 });
 
-class ProductCategorySelector extends Component {
+class CategorySelector extends React.Component {
     state = {
-        autoCompleteData: [],
+        single: '',
+        popper: '',
+        suggestions: [],
     };
 
-    constructor(props) {
-        super(props);
-        this.folderSelection = React.createRef();
-    }
+    handleSuggestionsFetchRequested = ({ value }) => {
+        this.setState({
+            suggestions: getSuggestions(value),
+        });
+    };
+
+    handleSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: [],
+        });
+    };
+
+    handleChange = name => (event, { newValue }) => {
+        this.setState({
+            [name]: newValue,
+        });
+    };
 
     render() {
-        const {classes} = this.props;
+        const { classes } = this.props;
 
-        return(
-            <div> say somthing
-            </div>
-        );
+        const autosuggestProps = {
+            renderInputComponent,
+            suggestions: this.state.suggestions,
+            onSuggestionsFetchRequested: this.handleSuggestionsFetchRequested,
+            onSuggestionsClearRequested: this.handleSuggestionsClearRequested,
+            getSuggestionValue,
+            renderSuggestion,
+        };
+
+        return (
+            <div className={classes.root}>
+    <Autosuggest
+        {...autosuggestProps}
+        inputProps={{
+            classes,
+                placeholder: 'Select Available Category',
+                value: this.state.single,
+                onChange: this.handleChange('single'),
+        }}
+        theme={{
+            container: classes.container,
+                suggestionsContainerOpen: classes.suggestionsContainerOpen,
+                suggestionsList: classes.suggestionsList,
+                suggestion: classes.suggestion,
+        }}
+        renderSuggestionsContainer={options => (
+        <Paper {...options.containerProps} square>
+        {options.children}
+    </Paper>
+    )}
+        />
+
+        </div>
+    );
     }
-
 }
 
-
-ProductCategorySelector.propTypes = {
+CategorySelector.propTypes = {
     classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(ProductCategorySelector);
+export default withStyles(styles)(CategorySelector);
